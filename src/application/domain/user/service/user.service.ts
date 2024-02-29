855430e98c1c93570696e22b2ee237248c7058e0
김주영 <103026521+jyk1029@users.noreply.github.com>
2024-02-29 14:45:49 +0900

    merge :: apply friend api (#14)
    * add :: apply friend api
    
    * chore :: delete log

---
+++ src/application/domain/user/service/user.service.ts
@@ -0,0 +1,23 @@
+import { HttpException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { FriendApplyEntity } from "../../../../infrastructure/domain/user/persistence/friend.apply.entity";
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(FriendApplyEntity)
+        private friendApplyRepository: Repository<FriendApplyEntity>
+    ) {
+    }
+
+    async applyFriend(req) {
+        let friendApply = new FriendApplyEntity();
+
+        friendApply.status = 'WAIT'
+        friendApply.requestUserId = req.authInfo.id
+        friendApply.receiveUserId = req.user.id
+
+        await this.friendApplyRepository.save(friendApply)
+    }
+}
