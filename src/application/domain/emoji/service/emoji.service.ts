22bd000439fd4f66c7e03fadaa6331a54e281ea2
김주영 <103026521+jyk1029@users.noreply.github.com>
2024-03-16 23:03:14 +0900

    merge :: emoji api (#23)
    * add :: query emoji api
    
    * chore :: code format

--- src/application/domain/emoji/service/emoji.service.ts
+++ src/application/domain/emoji/service/emoji.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { EmojiEntity } from "../../../../infrastructure/domain/emoji/persistence/emoji.entity";
-import { Repository } from "typeorm";
-import { CreateEmojiRequest } from "../dto/emoji.dto";
+import { Between, Repository } from "typeorm";
+import { CreateEmojiRequest, EmojiElement, QueryEmojiResponse } from "../dto/emoji.dto";
 
 @Injectable()
 export class EmojiService {
@@ -15,5 +15,33 @@ export class EmojiService {
     async createEmoji(req, request: CreateEmojiRequest) {
         let { title, content, imageUrl, price } = request
         await this.emojiRepository.save({ title: title, content: content, image: imageUrl, price: price, userId: req })
+    }
+
+    async queryEmojiFilter(min, max) {
+        let emojis = await this.emojiRepository.findBy({ price: Between(min, max) })
+        return this.queryEmoji(emojis)
+    }
+
+    async queryEmojiAll() {
+        let emojis = await this.emojiRepository.find()
+        return this.queryEmoji(emojis)
+    }
+
+    private async queryEmoji(emojis) {
+        let response = new QueryEmojiResponse()
+
+        response.emojis = await Promise.all(emojis.map(async (emoji) => {
+            let element = new EmojiElement()
+
+            element.id = emoji.id
+            element.title = emoji.title
+            element.content = emoji.content
+            element.image = emoji.image
+            element.price = emoji.price
+
+            return element
+        }))
+
+        return response
     }
 }
